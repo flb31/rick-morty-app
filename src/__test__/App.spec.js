eb6a6c93d17b93282ca208bf997f4d6ea0aa816c
Fabian Luna <fabian.luna@almundo.com>
2019-03-26 14:05:38 -0500

    Start Rick and Morty project

---
+++ src/__test__/App.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from '../App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
